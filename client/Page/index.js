ec0a3ae10d33de1f7cf3f1a8e8cbd0c4391150e5
王名旭 <1220712534@qq.com>
2018-08-30 09:58:03 +0800

    Commit

--- client/Page/index.js
+++ client/Page/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import api from 'utils/api';
 import { connect } from 'react-redux';
 import * as demoAction from 'Action/Demo';
-import '../styles/demo.less';
+import '../styles/index.less';
 import PropTypes from 'prop-types';
+import { NoticeBar } from 'antd-mobile';
+import laba from 'assets/campus-line/laba.png';
 
 @connect(
   state => ({ position: state.demo.position }),
@@ -13,17 +15,48 @@ class Index extends React.Component {
   static loadData = async (dispatch) => {
     const { data: position } = await api.getCity({ test: 1 });
     dispatch(demoAction.setPosition(position));
-  }
+  };
   static propTypes = {
     position: PropTypes.string.isRequired,
     dispatch: PropTypes.func.isRequired,
-  }
+  };
   componentDidMount() {
     this.constructor.loadData(this.props.dispatch);
   }
   render() {
-    const { position } = this.props;
-    return (<div styleName="demo">当前所在的城市为:{position}</div>);
+    return (
+      <div styleName="index-page">
+        <div styleName="banner" />
+        <NoticeBar
+          icon={<img src={laba} alt="laba" styleName="laba" />}
+          marqueeProps={{ loop: true, style: { color: '#80f2fa' } }}
+          styleName="notice-bar"
+        >
+          为你社团打CALL，寻找最美新声
+        </NoticeBar>
+        <div styleName="user-card">
+          <div styleName="user-info">
+            <img
+              src="http://wx.qlogo.cn/mmopen/fnOljJRc0roloB27t9a8Q1LaUNMxeYocs9lYDRaeG5JCeDvBVMVCLu6qZP76ibyuvB3TLGicqJpye8ZicTicr2YXKXficXXt3ejka/0"
+              alt="avatar"
+              styleName="avatar"
+            />
+            <div styleName="stat">
+              <div styleName="btn-login" />
+              <div styleName="btn-my_voice" />
+              <div styleName="rest-votes">剩余贡献值：10</div>
+              <div styleName="history-votes">
+                <div styleName="today">今日贡献：53244</div>
+                <div styleName="total">总贡献值：53244</div>
+              </div>
+            </div>
+          </div>
+          <div styleName="my-community">
+            <div styleName="title">我支持的社团</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
