922bccfe219a610dc0ecbb025ce8bc6a0cc6ed03
王名旭 <1220712534@qq.com>
2018-12-07 14:09:20 +0800

    commit

---
+++ client/Page/Loading.tsx
@@ -0,0 +1,173 @@
+import * as PosterActions from '@/Action/Poster';
+import * as ResultActions from '@/Action/Result';
+import analysisingImg from '@/assets/voicereport/bg-analysising.png';
+import loadingImg from '@/assets/voicereport/bg-loading.png';
+import Poster from '@/Component/Poster';
+import { loadingTips } from '@/constant';
+import { IApplicationState } from '@/Reducer';
+import { HttpAliasMap, IResult, IUserInfo } from '@/types';
+import { delay } from '@/utils/promisify';
+import ClientDetectService from '@lz-service/ClientDetectService';
+import { Toast } from 'antd-mobile';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { bindActionCreators } from 'redux';
+import '../styles/poster.less';
+
+interface IProps extends RouteComponentProps<{id: string}> {
+  setPosterData: (poster: string) => void;
+  pollResult: (analysisId: string) => any;
+  result: IResult;
+  userInfo: IUserInfo;
+  httpAlias: HttpAliasMap;
+  cdServ: ClientDetectService;
+}
+
+interface IState {
+  isGenerating: boolean;
+  showVoiceLayout: boolean;
+  loadingTipIndex: number;
+}
+
+class Loading extends PureComponent<IProps, IState> {
+  public readonly state: IState = {
+    isGenerating: false,
+    showVoiceLayout: false,
+    loadingTipIndex: 0,
+  };
+
+  public lTimer: number;
+
+  public tTimer: number;
+
+  get analysisId() {
+    const { match } = this.props;
+    return match.params.id;
+  }
+
+  public componentDidMount() {
+    this.lTimer = setInterval(() => {
+      this.setState({ showVoiceLayout: !this.state.showVoiceLayout });
+    }, 1000);
+    this.tTimer = setInterval(() => {
+      const { loadingTipIndex } = this.state;
+      if (loadingTipIndex < loadingTips.length - 1) {
+        this.setState({ loadingTipIndex: loadingTipIndex + 1 });
+      }
+    }, 1500);
+  }
+
+  public componentWillUnmount() {
+    clearInterval(this.lTimer);
+    clearInterval(this.tTimer);
+  }
+
+  public startTask = async () => {
+    const taskP = async () => {
+      const html2canvas = await this.loadHtml2canvas();
+      await this.loadData();
+      await new Promise((resolve) => {
+        this.setState({ isGenerating: true }, resolve);
+      });
+      const imgData = await this.GeneratePoster(html2canvas);
+      this.setState({ isGenerating: false });
+      if (this.props.cdServ.isWeiXin) {
+        try {
+          const {
+            data: { img },
+          } = await this.props.httpAlias.save(imgData.split(',')[1], {
+            headers: {
+              'Content-Type': 'text/plain',
+            },
+            timeout: 5000,
+          });
+          this.props.setPosterData(img);
+        } catch (error) {
+          this.props.setPosterData(imgData);
+        }
+      } else {
+        this.props.setPosterData(imgData);
+      }
+    };
+    const minWaitTime = +this.analysisId ? 4000 : 1500;
+    await Promise.all([taskP(), delay(minWaitTime)]);
+    this.props.history.replace(`/result/${this.analysisId}`);
+  };
+
+  public loadHtml2canvas = () => (
+    new Promise(resolve => {
+      require.ensure([], (require) => {
+        const html2canvas = require('html2canvas');
+        resolve(html2canvas);
+      }, console.log, 'html2canvas')
+    })
+  )
+
+  public loadData = async () => {
+    // 如果analysisId为0则已经在启动时(已录过)获取了数据
+    if (+this.analysisId) {
+      try {
+        await this.props.pollResult(this.analysisId);
+      } catch (error) {
+        Toast.fail('啊噢~录音被外星人偷走了重录一次吧 /（ToT)/~', Toast.SHORT);
+        await Promise.reject(error);
+      }
+    }
+  };
+
+  public GeneratePoster = async (html2canvas) => {
+    try {
+      const canvas = await html2canvas(document.getElementById('poster'), {
+        useCORS: true,
+        scale: this.props.cdServ.isAndroid ? window.devicePixelRatio : 1,
+      });
+      const imgData = canvas.toDataURL('image/jpeg');
+      if (imgData.slice(0, 15).length < 15) {
+        await Promise.reject(new Error('生成图片失败'));
+      }
+      return imgData;
+    } catch (error) {
+      Toast.fail(error, Toast.SHORT);
+      return Promise.reject(error);
+    }
+  };
+
+  public render() {
+    const { isGenerating, showVoiceLayout, loadingTipIndex } = this.state;
+    const { result, userInfo } = this.props;
+    return [
+      +this.analysisId ? (
+        <div styleName="analysising" key="analysising">
+          <img src={analysisingImg} alt="analysising" styleName="bg" onLoad={this.startTask} />
+          <div styleName="voice-layout" style={{ opacity: showVoiceLayout ? 1 : 0 }} />
+          <div styleName="tip">
+            {loadingTips[loadingTipIndex]!.map((item, i) => (
+              <div styleName="text" key={i}>
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div styleName="loading" key="loading">
+          <img src={loadingImg} alt="loading" styleName="bg" onLoad={this.startTask} />
+          <div styleName="notes" />
+        </div>
+      ),
+      isGenerating ? (
+        <Poster key="poster" displayType="img" reportData={result} userInfo={userInfo} showZiyaAdv={false}/>
+      ) : null,
+    ];
+  }
+}
+
+export default connect(
+  (state: IApplicationState) => ({
+    result: state.Result,
+    userInfo: state.UserInfo,
+    cdServ: state.Injector.get('cdServ'),
+    httpAlias: state.Injector.get('$http').alias,
+  }),
+  dispatch => bindActionCreators({ ...ResultActions, ...PosterActions }, dispatch),
+)(Loading);
---
+++ client/Page/Voice.tsx
@@ -0,0 +1,160 @@
+import * as ResultActions from '@/Action/Result';
+import avatar from '@/assets/avatar';
+import { IApplicationState } from '@/Reducer';
+import { IResult, IUserInfo } from '@/types';
+import { stopPropagation } from '@/utils/domHelper';
+import { showDownloadDialog } from '@/utils/openApp';
+import { Toast } from 'antd-mobile';
+import classNames from 'classnames';
+import AudioPlayerService, {
+  AudioStatus,
+  EventMap as AudioEventMap,
+} from 'di-sdk/package/AudioPlayerService';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { bindActionCreators } from 'redux';
+import '../styles/voice.less';
+
+interface IProps extends RouteComponentProps<{ id: string }> {
+  result: IResult;
+  userInfo: IUserInfo;
+  player: AudioPlayerService;
+  pollResult: (analysisId: string) => any;
+}
+
+interface IState {
+  status: AudioStatus;
+  showAudioCtrlBtn: boolean;
+}
+
+class Voice extends PureComponent<IProps, IState> {
+  public state = {
+    status: AudioStatus.WAIT_PLAY,
+    showAudioCtrlBtn: false,
+  };
+
+  public async componentDidMount() {
+    const { player, result: {voiceMan: {voiceManAudioUrl}} } = this.props;
+    // 兼容从外部浏览器打开丢失redux store里面的数据
+    if (!voiceManAudioUrl) {
+      await this.props.pollResult(this.analysisId);
+    }
+    player.on(AudioEventMap.STATUS_CHANGE, this.handleAudioStatusChange);
+    player.on(AudioEventMap.ERROR, this.handleAudioError);
+    !player.audioRef.src && player.setAudioSrc(this.props.result.voiceMan.voiceManAudioUrl, false)
+    if (player.audioStatus !== AudioStatus.PLAYING) {
+      Toast.info('正在加载音频...', 0);
+      try {
+        await player.play();
+      } finally {
+        Toast.hide();
+      }
+    }
+  }
+
+  public componentWillUnmount() {
+    const { player } = this.props;
+    player.pause();
+    player.off(AudioEventMap.STATUS_CHANGE, this.handleAudioStatusChange);
+    player.off(AudioEventMap.ERROR, this.handleAudioError);
+  }
+
+  public handleAudioError = (error) => {
+    console.log(error);
+    this.setState({ showAudioCtrlBtn: true });
+  };
+
+  public handleAudioStatusChange = (status) => {
+    this.setState({ status });
+    if (status === AudioStatus.WAIT_PLAY) {
+      this.setState({ showAudioCtrlBtn: true });
+    }
+  };
+
+  get analysisId() {
+    const { match } = this.props;
+    return match.params.id;
+  }
+
+  public testFate = () => {
+    const [matchstr, userId, id] = this.props.result.voiceMan.voiceManLink.match(/\/(\d+)\/(\d+)$/);
+    console.log(matchstr);
+    const action = {
+      type: 28,
+      id, // 声音 id
+      extraData: {
+        userId, // 主播 id
+      },
+    };
+    showDownloadDialog(action);
+  };
+
+  public toggleAudioBtn = () => {
+    const { status } = this.state;
+    if (status === AudioStatus.PLAYING) {
+      this.setState({
+        showAudioCtrlBtn: !this.state.showAudioCtrlBtn,
+      });
+    }
+  };
+
+  public play = () => {
+    this.props.player.play();
+    this.setState({
+      showAudioCtrlBtn: false,
+    });
+  };
+
+  public pause = () => {
+    this.props.player.pause();
+  };
+
+  public render() {
+    const { status, showAudioCtrlBtn } = this.state;
+    const { result, userInfo } = this.props;
+    return (
+      <div styleName={classNames('page', { showAudioCtrlBtn })} onClick={this.toggleAudioBtn}>
+        {userInfo.name ? (
+          <div styleName="title">
+            {userInfo.name}
+            <br />
+            {' '}
+            专属声音气质报告
+          </div>
+        ) : null}
+        <div styleName={classNames('player', { isPlaying: status === AudioStatus.PLAYING })}>
+          <div styleName="rocker" />
+          {status !== AudioStatus.PLAYING ? (
+            <div styleName="btn-play" onClick={stopPropagation(this.play)} />
+          ) : (
+            <div styleName="btn-pause" onClick={stopPropagation(this.pause)} />
+          )}
+          {result.voiceMan.matchType > 0 ? (
+            <img
+              src={avatar[result.voiceMan.matchType - 1]}
+              alt="avatar"
+              styleName="avatar"
+            />
+          ) : null}
+          <div styleName="static-round">
+            <div styleName="dash" />
+          </div>
+          <div styleName="round1" />
+          <div styleName="round2" />
+          <div styleName="round3" />
+          <div styleName="btn-test-fate" onClick={stopPropagation(this.testFate)} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  (state: IApplicationState) => ({
+    result: state.Result,
+    userInfo: state.UserInfo,
+    player: state.Injector.get('player', {}),
+  }),
+  dispatch => bindActionCreators(ResultActions, dispatch),
+)(Voice);
